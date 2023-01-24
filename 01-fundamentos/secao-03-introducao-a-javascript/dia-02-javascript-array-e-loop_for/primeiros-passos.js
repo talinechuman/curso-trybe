651feef82d517859e4230ca2b073def5099f2312
Taline Chuman <talinechuman@hotmail.com>
2023-01-24 19:03:48 -0300

    Exercicio 01

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/primeiros-passos.js
@@ -0,0 +1,38 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+// // [ 'Tomar café', 'Reunião' ]
+
+
+// Obtenha o valor “Serviços” do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+// console.log(menuServices);
+
+//Procure o índice do valor “Portfólio” do array menu:
+// let menu = ['Home', 'Serviços', 'Portifólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portifólio');
+// console.log(indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push ('Contato')
+// console.log(menu);
+
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (let index = 0; index < groceryList.length; index++) {
+//     console.log (groceryList[index]);
+// }
+
+// var numero = 5;
+// for (var contador = 1; contador <=9; contador++) {
+//     console.log ("");
+//     console.log (numero * contador);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for(let i of names) {
+//   console.log(i);
+// }
+
