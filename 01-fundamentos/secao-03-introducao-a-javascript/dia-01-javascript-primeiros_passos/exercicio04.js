3ecd53788c28bb8f91a42d03ce18bac053684d81
Taline Chuman <talinechuman@hotmail.com>
2023-01-23 21:11:51 -0300

    Exercicio 04

---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros_passos/exercicio04.js
@@ -0,0 +1,5 @@
+// 🚀 Utilize if/else para escrever um código que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
