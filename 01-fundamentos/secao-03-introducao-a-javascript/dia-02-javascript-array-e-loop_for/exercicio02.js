ebfc72b47e70e15ffb51e06578f0564a77f5ee60
Taline Chuman <talinechuman@hotmail.com>
2023-01-24 21:37:26 -0300

    Exercicio 02

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio02.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio02.js
@@ -1,2 +1,10 @@
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+soma = 0;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (i = 0; i < numbers.length ; i++){
+    soma = soma + numbers [i];
+}
+
+console.log (soma);
 
