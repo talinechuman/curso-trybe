adbc3450449e24803f91e56e7227d0df58b2b7ad
Taline Chuman <talinechuman@hotmail.com>
2023-01-24 22:21:08 -0300

    Exercicio 06

--- 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio06.js
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio06.js
@@ -1,7 +1,18 @@
 
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
-// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
-// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let numbers = [2,2,6,3];
+
+somaImpar = 0;
+
+for (i=0; i < numbers.length; i++){
+    if (numbers[i]%2 !=0){
+    somaImpar = somaImpar + 1;
+    }
+}
+if (somaImpar == 0){
+    console.log("Nenhum valor impar encontrado");
+} else {
+    console.log (somaImpar);
+}
---
+++ 01-fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/exercicio07.js
@@ -0,0 +1,6 @@
+
+// Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+// Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
